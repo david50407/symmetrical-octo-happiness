2c3383bf5f7d3a4c66dbc4e41c27bb4f838c882e
David Kuo <s50407s@gmail.com>
2018-03-01 14:20:23 +0800

    Auto backup via Travis CI

---
+++ .travis-build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -e
+
+rev=$(git rev-parse --short HEAD)
+
+git config --global user.email "me@davy.tw"
+git config --global user.name "Travis on behalf David Kuo"
+
+wget -O index.md https://hackmd.io/BJ5BvWSOf/download
+
+git submodule init
+git submodule add -b gh-pages -f --name build "https://$GH_TOKEN@github.com/david50407/symmetrical-octo-happiness.git" build
+
+mv index.md build/index.md
+echo "$(git rev-parse HEAD)" > build/.rev
+
+cd build; git add -fA .; git commit -m "Build at ${rev} [ci skip]"; git push -q || true
